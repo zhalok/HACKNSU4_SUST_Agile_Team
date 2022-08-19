f51c002fbee6ef777e247d5714a2a967219ef448
zhalok <zhalok>
2022-08-19 10:37:56 +0600

    update

--- server/controllers/user-route-controller.js
+++ server/controllers/user-route-controller.js
@@ -1,5 +1,5 @@
-const mysql = require("mysql");
 const user_route_controller = {};
 user_route_controller.add_user = (req, res, next) => {
-  const { name, email, address, city, state, country, phone } = req.body;
+  const { name, email, address, city, state, country, phone, password } =
+    req.body;
 };
