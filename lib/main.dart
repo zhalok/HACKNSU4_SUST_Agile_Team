bd0332a6cdd8b7cdb6d74fe1f15ad7c609ef570a
Riddhiman Debnath <riddimandebnath4260@gmail.com>
2022-08-19 12:09:21 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,104 @@
-// ignore_for_file: prefer_const_constructors
+// // ignore_for_file: prefer_const_constructors
+
+// import 'package:flutter/material.dart';
+// void main(){
+//   runApp(const MyApp());
+// }
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   String dropdownvalue = 'Dhaka';
+
+//   var items = [
+//     'Dhaka',
+//     'Sylhet',
+//     'Chittagong',
+//     'Rajshahi',
+//     'Khulna',
+//     'Mymensingh'
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       padding: const EdgeInsets.all(10),
+//       decoration:const BoxDecoration(
+//     color: Colors.white,
+//     borderRadius: BorderRadius.only(
+//       topLeft: Radius.circular(10),
+//         topRight: Radius.circular(10),
+//         bottomLeft: Radius.circular(10),
+//         bottomRight: Radius.circular(10)
+//     ),
+//       ),
+//       child:  Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             DropdownButton(
+//               isExpanded: false,
+//               value: dropdownvalue,
+//               icon: const Icon(Icons.keyboard_arrow_down),
+//               items: items.map((String items) {
+//                 return DropdownMenuItem(
+//                   value: items,
+//                   child: Text(items),
+//                 );
+//               }).toList(),
+//               onChanged: (String? newValue) {
+//                 setState(() {
+//                   dropdownvalue = newValue!;
+//                 });
+//               },
+//               hint: Align(
+//                 alignment: Alignment.centerLeft,
+//                 child: Text(
+//                   "From",
+//                   style: TextStyle(color: Colors.black),
+//                 ),
+//               ),
+//               ),
+//               DropdownButton(
+//               isExpanded: false,
+//               value: dropdownvalue,
+//               icon: const Icon(Icons.keyboard_arrow_down),
+//               items: items.map((String items) {
+//                 return DropdownMenuItem(
+//                   value: items,
+//                   child: Text(items),
+//                 );
+//               }).toList(),
+//               onChanged: (String? newValue) {
+//                 setState(() {
+//                   dropdownvalue = newValue!;
+//                 });
+//               },
+//               hint: Align(
+//                 alignment: Alignment.centerLeft,
+//                 child: Text(
+//                   "To",
+//                   style: TextStyle(color: Colors.black),
+//                 ),
+//               ),
+//               ),
+
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 import 'package:flutter/material.dart';
-void main(){
+
+void main(List<String> args) {
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -12,83 +107,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String dropdownvalue = 'Dhaka';  
- 
-  
-  var items = [   
-    'Dhaka',
-    'Sylhet',
-    'Chittagong',
-    'Rajshahi',
-    'Khulna',
-    'Mymensingh'
-  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration:const BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.only(
-      topLeft: Radius.circular(10),
-        topRight: Radius.circular(10),
-        bottomLeft: Radius.circular(10),
-        bottomRight: Radius.circular(10)
-    ),
-      ),
-      child:  Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButton(
-              isExpanded: false,
-              value: dropdownvalue,
-              icon: const Icon(Icons.keyboard_arrow_down),
-              items: items.map((String items) {
-                return DropdownMenuItem(
-                  value: items,
-                  child: Text(items),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownvalue = newValue!;
-                });
-              },
-              hint: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "From",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              ),
-              DropdownButton(
-              isExpanded: false,
-              value: dropdownvalue,
-              icon: const Icon(Icons.keyboard_arrow_down),
-              items: items.map((String items) {
-                return DropdownMenuItem(
-                  value: items,
-                  child: Text(items),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownvalue = newValue!;
-                });
-              },
-              hint: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "To",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              ),
-              
-          ],
-        ),
+    return MaterialApp(
+      title: 'E-Ticket',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.grey),
+      color: Colors.amberAccent,
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text(
+          'E-Ticket',
+          style: TextStyle(color: Colors.black),
+        )),
       ),
     );
   }
